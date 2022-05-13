2668d93ebb16213ede500ebc5d78a2cf35288263
PierreGatschine <pierre.gatschine@gmail.com>
2022-05-13 11:27:18 +0200

    created authentication page component

--- src/components/sign-up-form copy/sign-up-form.component.jsx
+++ src/components/sign-up-form copy/sign-up-form.component.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import FormInput from "../form-input/form-input.component";
-import Button from '../../components/button/button.component';
+import Button from '../button/button.component';
 
 import './sign-up-form.styles.scss'
 
