142cb8b91abd3b62dfd7d363ab8d59f2d52f5f45
PierreGatschine <pierre.gatschine@gmail.com>
2022-05-16 17:22:11 +0200

    Add product-card

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,12 +1,17 @@
-import SHOP_DATA from '../../data/shop.data';
+import { useContext } from "react";
 
-import './Shop.styles.scss';
+import { ProductsContext } from "../../contexts/products.context";
+import ProductCard from "../../components/product-card/product-card.component";
+
+
+import './shop.styles.scss';
 
 const Shop = () => {
+    const { products } = useContext(ProductsContext);
     return (
-        <div>
-            {SHOP_DATA.map(({ id, name }) => (
-                <div key={id}>{name}</div>
+        <div className="products-container">
+            {products.map((product) => (
+                <ProductCard key={product.id} product={product} />
             ))}
         </div>
     );
