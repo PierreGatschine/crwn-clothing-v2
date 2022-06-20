afd0553c3293fa5d1d516cca6cd0d3c076a7ed6e
PierreGatschine <pierre.gatschine@gmail.com>
2022-06-20 15:21:47 +0200

    add saga-categories

--- src/routes/category/category.component.jsx
+++ src/routes/category/category.component.jsx
@@ -3,14 +3,17 @@ import { useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
 
 import ProductCard from '../../components/product-card/product-card.component';
-import { selectCategoriesMap } from '../../store/categories/category.selector';
+import Spinner from "../../components/spinner/spinner.component";
+
+import { selectCategoriesMap, selectCategoriesIsLoading } from '../../store/categories/category.selector';
 //import { CategoriesContext } from '../../contexts/categories.context';
 
-import './category.styles.scss';
+import { CategoryContainer, Title } from './category.styles';
 
 const Category = () => {
     const { category } = useParams();
     const categoriesMap = useSelector(selectCategoriesMap)
+    const isLoading = useSelector(selectCategoriesIsLoading)
     const [products, setProducts] = useState(categoriesMap[category]);
     //console.log('render/re-rendering category component');
 
@@ -20,18 +23,22 @@ const Category = () => {
         setProducts(products);
     }, [category, categoriesMap]);
 
-    return (
-        <Fragment>
-            <h2 className='category-title'>{category.toUpperCase()}</h2>
-            <div className='category-container'>
-                {
-                    products &&
-                    products.map(product =>  (
-                    <ProductCard key={product.id} product={product} />
-                ))}
-            </div>
-        </Fragment>
-    );
+   return (
+    <Fragment>
+      <Title>{category.toUpperCase()}</Title>
+      {isLoading ? (
+        <Spinner />
+      ) : (
+        <CategoryContainer>
+          {products &&
+            products.map((product) => (
+              <ProductCard key={product.id} product={product} />
+            ))}
+        </CategoryContainer>
+      )}
+    </Fragment>
+  );
 };
+
 
 export default Category;
--- src/store/store.js
+++ src/store/store.js
@@ -2,32 +2,19 @@ import { compose, createStore, applyMiddleware } from 'redux';
 import { persistStore, persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 import logger from 'redux-logger';
+//import thunk from 'redux-thunk';
+import createSagaMiddleware from 'redux-saga';
+
+import { rootSaga } from './root-saga';
 
 import { rootReducer } from './root-reducer';
 
-/* const loggerMiddleware = (store) => (next) => (action) => {
-  if (!action.type) {
-    return next(action);
-  }
+const sagaMidleware = createSagaMiddleware();
 
-  console.log('type: ', action.type);
-  console.log('payload: ', action.payload);
-  console.log('currentState: ', store.getState());
-
-  next(action);
-
-  console.log('next state: ', store.getState());
-}; */
-
-const persistConfig = {
-  key: 'root',
-  storage,
-  blackList: ['user'],
-};
-
-const persistedReducer = persistReducer(persistConfig, rootReducer);
-
-const middlewares = [process.env.NODE_ENV !== 'production' && logger].filter(Boolean);
+const middlewares = [
+  process.env.NODE_ENV !== 'production' && logger,
+  sagaMidleware,
+].filter(Boolean);
 
 const composeEnhancer =
   (process.env.NODE_ENV !== 'production' &&
@@ -37,8 +24,24 @@ const composeEnhancer =
 
 
 
+const persistConfig = {
+  key: 'root',
+  storage,
+  //blackList: ['user'],
+  whitelist: ['cart'],
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+
 const composedEnhancers =  composeEnhancer(applyMiddleware(...middlewares));
 
-export const store = createStore(persistedReducer, undefined, composedEnhancers);
+export const store = createStore(
+  persistedReducer,
+  undefined, 
+  composedEnhancers
+);
+
+sagaMidleware.run(rootSaga);
 
 export const persistor = persistStore(store);
