cbdbdf96c6af1cd6e2cfc115091f59fa7d2df27a
PierreGatschine <pierre.gatschine@gmail.com>
2022-06-21 14:46:37 +0200

    Add sign-in-redux-saga

--- src/components/sign-in-form/sign-in-form.component.jsx
+++ src/components/sign-in-form/sign-in-form.component.jsx
@@ -1,17 +1,17 @@
-import { useState, /* useContext */ } from "react";
+import { useState } from "react";
+import { useDispatch } from "react-redux";
 
 import FormInput from "../form-input/form-input.component";
 import Button, { BUTTON_TYPES_CLASSES } from '../button/button.component';
-//import { UserContext } from "../../contexts/user.context";
-
-import './sign-in-form.styles.scss'
 
 
+import { SignInContainer, ButtonsContainer } from './sign-in-form.styles';
 import {
-  signInWithGooglePopup,
-  //createUserDocumentFromAuth,
-  signInAuthUserWithEmailAndPassword,
-} from '../../utils/firebase/firebase.utils';
+    googleSignInStart,
+    emailSignInStart
+} from '../../store/user/user.action';
+
+import './sign-in-form.styles.jsx';
 
 const defaultFormFields = {
     email: "",
@@ -19,7 +19,7 @@ const defaultFormFields = {
 }
 
 const SignInForm = () => {
-
+    const dispatch = useDispatch();
     const [formFields, setFormFields] = useState(defaultFormFields);
     const { email, password } = formFields;
 
@@ -30,10 +30,7 @@ const SignInForm = () => {
     }
 
     const signInWithGoogle = async () => {
-        await signInWithGooglePopup();
-        /* const { user } = await signInWithGooglePopup();
-        await createUserDocumentFromAuth(user); */
-        //setCurrentUser(user);
+       dispatch(googleSignInStart());
     };
 
     const handleSubmit = async (event) => {
@@ -42,13 +39,8 @@ const SignInForm = () => {
         
 
         try {
-            const { user } = await signInAuthUserWithEmailAndPassword(
-                email,
-                password
-            );
-            console.log({ user });
+            dispatch(emailSignInStart(email, password));
             resetFormFields();
-            //setCurrentUser(user);
         } catch (error) {
             switch (error.code) {
                 case 'auth/wrong-password':
@@ -72,7 +64,7 @@ const SignInForm = () => {
 }
 
     return (
-        <div className="sign-up-container">
+        <SignInContainer>
             <h2>Already have an account ?</h2>
             <span>Sign in with your email and password</span>
             <form onSubmit={handleSubmit}>
@@ -92,7 +84,7 @@ const SignInForm = () => {
                     onChange={handleChange} 
                     name="password" 
                     value={password}/>
-                <div className="buttons-container">
+                <ButtonsContainer>
                     <Button type="submit">Sign In</Button>
                     <Button 
                         type='button' 
@@ -101,10 +93,10 @@ const SignInForm = () => {
                     >
                         Google sign in
                     </Button>
-                </div>
+                </ButtonsContainer>
 
             </form>
-        </div>
+        </SignInContainer>
     );
 };
 
